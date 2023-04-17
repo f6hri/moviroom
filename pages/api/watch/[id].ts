b21bf077cd349bb0a2ac1d1ad259c47c881c36d7
f6hri <fahrimdemir@hotmail.com>
2023-04-17 13:51:53 +0300

    +

--- pages/api/watch/[id].ts
+++ pages/api/watch/[id].ts
@@ -2,6 +2,10 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { getDoc, doc, DocumentData } from "firebase/firestore";
 import { db } from '@/config/db';
 
+type EposideContent = {
+  season: number;
+  eposides: Eposide[];
+}
 type Eposide = {
   title: string;
   name: string;
@@ -12,11 +16,21 @@ type Error = {
   message: string
 }
 
-const handler = async (req: NextApiRequest, res: NextApiResponse<Eposide[] | Error>) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse<EposideContent[] | Error>) => {
   const id: any = req.query.id;
   await getDoc(doc(db, "series", id))
     .then((querySnapshot: DocumentData) => {
-      res.status(200).json(querySnapshot.data().eposides)
+      const eposidesData = querySnapshot.data().eposides;
+      const eposidesCount = querySnapshot.data().details.number_of_seasons;
+      const eposidesArray:EposideContent[] = []
+      for (let i = 1; i <= eposidesCount; i++) {
+        const eposideContent = eposidesData.filter((eposide: Eposide) => parseInt(eposide.title.charAt(0)) === i)
+        eposidesArray.push({
+          season: i,
+          eposides: eposideContent
+        });
+      }
+      res.status(200).json(eposidesArray);
     }).catch((err) => {
       res.status(400).json({
         errorCode: err.code,
--- pages/watch/[id].tsx
+++ pages/watch/[id].tsx
@@ -4,6 +4,8 @@ import { Container } from "@/components/Layout";
 import Navbar from "@/components/Navbar";
 import { useRouter } from "next/router";
 import Loader from "@/components/Shared/Loader";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 type Eposide = {
   title: string;
@@ -11,15 +13,20 @@ type Eposide = {
   embedUrl: string;
 };
 
+type EposideContent = {
+  season: number;
+  eposides: Eposide[];
+};
+
 const Watch: React.FC = () => {
-  const [eposides, setEposides] = useState<Eposide[]>([]);
+  const [eposidesContent, setEposides] = useState<EposideContent[]>([]);
   const [video, setVideo] = useState<Eposide>({
     title: "",
     name: "",
     embedUrl: "",
   });
   const router = useRouter();
-  const id: any = router.query.id;
+  const { id }: any = router.query;
   const fetchEposides = async () => {
     const response = await fetch("/api/watch/" + id);
     if (!response.ok) {
@@ -27,7 +34,7 @@ const Watch: React.FC = () => {
     }
     const eposidesJson = await response.json();
     setEposides(eposidesJson);
-    setVideo(eposidesJson[0]);
+    setVideo(eposidesJson[0].eposides[0]);
   };
   const openVideo = (video: Eposide) => {
     setVideo(video);
@@ -41,7 +48,7 @@ const Watch: React.FC = () => {
         <title>{video.name || "Yükleniyor..."}</title>
       </Head>
       <Navbar />
-      {eposides.length === 0 ? (
+      {eposidesContent.length === 0 ? (
         <div className={`flex justify-center mt-10`}>
           <Loader />
         </div>
@@ -60,24 +67,36 @@ const Watch: React.FC = () => {
               <div
                 className={`w-full h-full border border-slate-800 rounded-lg`}
               >
-                <h3 className={`mx-4 py-4 border-b border-slate-800 text-xl`}>
+                <h3 className={`mx-4 py-4 text-xl`}>
                   Bölümler
                 </h3>
-                <ul className={`overflow-hidden overflow-y-auto h-[400px] p-3`}>
-                  {eposides.map((item: any, index: number) => (
-                    <li
-                      key={index}
-                      onClick={() => openVideo(item)}
-                      className={`${
-                        item.embedUrl === video.embedUrl &&
-                        "bg-primary/75 pointer-events-none"
-                      } flex cursor-pointer justify-between align-center py-3 border-b-1 hover:bg-primary px-2 rounded-md`}
-                    >
-                      <p>{item.title}</p>
-                      <p>{item.name}</p>
-                    </li>
-                  ))}
-                </ul>
+                <Tabs>
+                  <TabList className={`flex justify-center`}>
+                    {eposidesContent.map((item, index) => (
+                      <Tab className={`focus:bg-primary focus:text-[#fff] click:bg-primary cursor-pointer p-3 py-2 rounded-lg`} key={index}>S{item.season}</Tab>
+                    ))}
+                  </TabList>
+                  {
+                    eposidesContent.map((eposides, index) => (
+                      <TabPanel key={index}>
+                        <ul className={`overflow-hidden overflow-y-auto h-[400px] p-3`}>
+                          {eposides.eposides.map((item: any, index: number) => (
+                            <li
+                              key={index}
+                              onClick={() => openVideo(item)}
+                              className={`${item.embedUrl === video.embedUrl &&
+                                "bg-primary/75 pointer-events-none"
+                                } flex cursor-pointer justify-between align-center py-3 border-b-1 hover:bg-primary px-2 rounded-md`}
+                            >
+                              <p>{item.title}</p>
+                              <p>{item.name}</p>
+                            </li>
+                          ))}
+                        </ul>
+                      </TabPanel>
+                    ))
+                  }
+                </Tabs>
               </div>
             </div>
           </div>
