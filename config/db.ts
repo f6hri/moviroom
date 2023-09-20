b3fca3b4e65cf18c08e9a7d3fc2b73eec881361e
f6hri <fahrimdemir@hotmail.com>
2023-09-20 02:55:11 -0700

    +

--- config/db.ts
+++ config/db.ts
@@ -3,12 +3,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Insert Your Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyAQyAueuwXC2eOM0hXeBDx8msbKUUP6Lxg",
-  authDomain: "moviroom-a3663.firebaseapp.com",
-  projectId: "moviroom-a3663",
-  storageBucket: "moviroom-a3663.appspot.com",
-  messagingSenderId: "539481957305",
-  appId: "1:539481957305:web:8d36e92cce80f1ffb28c"
+  // CONFIG
 };
 
 const app = initializeApp(firebaseConfig);
